b9ea11a88ca50fc2277fe014223c6de6a2d8daef
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-05-29 00:04:29 +0530

    Anki Vector

--- AnkiVector/modules/disasters.py
+++ AnkiVector/modules/disasters.py
@@ -7,10 +7,10 @@ from telegram import ParseMode, TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 from telegram.utils.helpers import mention_html
 
-from DaisyX import DEMONS, DEV_USERS, DRAGONS, OWNER_ID, TIGERS, WOLVES, dispatcher
-from DaisyX.modules.helper_funcs.chat_status import dev_plus, sudo_plus, whitelist_plus
-from DaisyX.modules.helper_funcs.extraction import extract_user
-from DaisyX.modules.log_channel import gloggable
+from AnkiVector import DEMONS, DEV_USERS, DRAGONS, OWNER_ID, TIGERS, WOLVES, dispatcher
+from AnkiVector.modules.helper_funcs.chat_status import dev_plus, sudo_plus, whitelist_plus
+from AnkiVector.modules.helper_funcs.extraction import extract_user
+from AnkiVector.modules.log_channel import gloggable
 
 ELEVATED_USERS_FILE = os.path.join(os.getcwd(), "DaisyX/elevated_users.json")
 
