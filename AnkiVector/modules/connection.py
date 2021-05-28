3c40e54e44a49f257a5590004d9688c169a8a28d
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-05-28 23:51:37 +0530

    Update connection.py

--- AnkiVector/modules/connection.py
+++ AnkiVector/modules/connection.py
@@ -5,10 +5,10 @@ from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CallbackQueryHandler, CommandHandler, run_async
 
-import DaisyX.modules.sql.connection_sql as sql
-from DaisyX import DEV_USERS, DRAGONS, dispatcher
-from DaisyX.modules.helper_funcs import chat_status
-from DaisyX.modules.helper_funcs.alternate import send_message, typing_action
+import AnkiVector.modules.sql.connection_sql as sql
+from AnkiVector import DEV_USERS, DRAGONS, dispatcher
+from AnkiVector.modules.helper_funcs import chat_status
+from AnkiVector.modules.helper_funcs.alternate import send_message, typing_action
 
 user_admin = chat_status.user_admin
 
