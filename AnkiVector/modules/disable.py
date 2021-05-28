b67daaeb98bc1790dee46d5fe5b8e1271df23751
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-05-29 00:01:47 +0530

    Update disable.py

--- AnkiVector/modules/disable.py
+++ AnkiVector/modules/disable.py
@@ -12,9 +12,9 @@ from telegram.ext import (
 )
 from telegram.utils.helpers import escape_markdown
 
-from DaisyX import dispatcher
-from DaisyX.modules.helper_funcs.handlers import CMD_STARTERS, SpamChecker
-from DaisyX.modules.helper_funcs.misc import is_module_loaded
+from AnkiVector import dispatcher
+from AnkiVector.modules.helper_funcs.handlers import CMD_STARTERS, SpamChecker
+from AnkiVector.modules.helper_funcs.misc import is_module_loaded
 
 FILENAME = __name__.rsplit(".", 1)[-1]
 
@@ -23,7 +23,7 @@ if is_module_loaded(FILENAME):
 
     from telegram.ext.dispatcher import run_async
 
-    from DaisyX.modules.helper_funcs.chat_status import (
+    from AnkiVector.modules.helper_funcs.chat_status import (
         connection_status,
         is_user_admin,
         user_admin,
