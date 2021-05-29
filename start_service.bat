3fb5e615e5f296bc5bf82fa0b5e00d2e2397618d
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-05-29 15:20:56 +0530

    Anki Vector

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop DaisyX
-net start DaisyX
+net stop AnkiVector
+net start AnkiVector
