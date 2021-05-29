41f9738f1fb7d40e2bcc6703acf975fcafda5495
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-05-29 11:57:03 +0530

    Anki Vector

--- AnkiVector/modules/telethnfun.py
+++ AnkiVector/modules/telethnfun.py
@@ -9,8 +9,8 @@ from bing_image_downloader import downloader
 from requests import get, post
 from telethon.tl import functions, types
 
-from DaisyX import telethn as client
-from DaisyX.events import register
+from AnkiVector import telethn as client
+from AnkiVector.events import register
 
 
 def progress(current, total):
