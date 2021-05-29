8d2df770548ec9297f85a289e97560f36a1d3861
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-05-29 10:56:41 +0530

    Anki Vector

--- AnkiVector/modules/gtranslator.py
+++ AnkiVector/modules/gtranslator.py
@@ -3,8 +3,8 @@ from googletrans import LANGUAGES, Translator
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
-from DaisyX import dispatcher
-from DaisyX.modules.disable import DisableAbleCommandHandler
+from AnkiVector import dispatcher
+from AnkiVector.modules.disable import DisableAbleCommandHandler
 
 
 @run_async
