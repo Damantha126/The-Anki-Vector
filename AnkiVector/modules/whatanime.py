2ad92c6c7a22e92b8e07272b047bbb6e59c1299d
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-05-29 12:18:40 +0530

    Anki Vector

--- AnkiVector/modules/whatanime.py
+++ AnkiVector/modules/whatanime.py
@@ -12,7 +12,7 @@ import aiohttp
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from DaisyX import pbot
+from AnkiVector import pbot
 
 session = aiohttp.ClientSession()
 progress_callback_data = {}
