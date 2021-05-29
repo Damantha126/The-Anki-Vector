bd0a09cf94b75c132a546570783e937139558623
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-05-29 11:34:41 +0530

    Anki Vector

--- AnkiVector/modules/shell.py
+++ AnkiVector/modules/shell.py
@@ -4,8 +4,8 @@ from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.ext.dispatcher import run_async
 
-from DaisyX import LOGGER, dispatcher
-from DaisyX.modules.helper_funcs.chat_status import dev_plus
+from AnkiVector import LOGGER, dispatcher
+from AnkiVector.modules.helper_funcs.chat_status import dev_plus
 
 
 @dev_plus
