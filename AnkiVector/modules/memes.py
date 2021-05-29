e7d00799de7ce919ae6069c4c749d9dc3a2caed8
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-05-29 11:08:01 +0530

    Anki Vector

--- AnkiVector/modules/memes.py
+++ AnkiVector/modules/memes.py
@@ -9,8 +9,8 @@ from PIL import Image
 from spongemock import spongemock
 from telegram.ext import run_async
 
-from DaisyX import dispatcher
-from DaisyX.modules.disable import DisableAbleCommandHandler
+from AnkiVector import dispatcher
+from AnkiVector.modules.disable import DisableAbleCommandHandler
 
 
 @run_async
