afd2e706da7fb42a6f579e1d3690c05aff9f291a
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-05-29 14:52:58 +0530

    Anki Vector

--- AnkiVector/modules/helper_funcs/extraction.py
+++ AnkiVector/modules/helper_funcs/extraction.py
@@ -5,8 +5,8 @@ from telegram.error import BadRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 
-from DaisyX import LOGGER
-from DaisyX.modules.users import get_user_id
+from AnkiVector import LOGGER
+from AnkiVector.modules.users import get_user_id
 
 
 def id_from_reply(message):
