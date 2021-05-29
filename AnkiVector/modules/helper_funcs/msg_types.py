09f34fcae82d4e337c0116af271984dedba328d4
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-05-29 14:55:45 +0530

    Anki Vector

--- AnkiVector/modules/helper_funcs/msg_types.py
+++ AnkiVector/modules/helper_funcs/msg_types.py
@@ -2,7 +2,7 @@ from enum import IntEnum, unique
 
 from telegram import Message
 
-from DaisyX.modules.helper_funcs.string_handling import button_markdown_parser
+from AnkiVector.modules.helper_funcs.string_handling import button_markdown_parser
 
 
 @unique
