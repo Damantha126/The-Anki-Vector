212ae95377b2baa78740a45730b1b7c4e6a4ae9e
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-05-29 14:35:48 +0530

    Anki Vector

--- AnkiVector/modules/sql/feds_sql.py
+++ AnkiVector/modules/sql/feds_sql.py
@@ -3,8 +3,8 @@ import threading
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 from telegram.error import BadRequest, Unauthorized
 
-from DaisyX import dispatcher
-from DaisyX.modules.sql import BASE, SESSION
+from AnkiVector import dispatcher
+from AnkiVector.modules.sql import BASE, SESSION
 
 
 class Federations(BASE):
