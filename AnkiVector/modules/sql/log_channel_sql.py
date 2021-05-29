62546adc5562096dce1adcce40ee0007a9767bc2
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-05-29 14:38:24 +0530

    Anki Vector

--- AnkiVector/modules/sql/log_channel_sql.py
+++ AnkiVector/modules/sql/log_channel_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, distinct, func
 
-from DaisyX.modules.sql import BASE, SESSION
+from AnkiVector.modules.sql import BASE, SESSION
 
 
 class GroupLogs(BASE):
