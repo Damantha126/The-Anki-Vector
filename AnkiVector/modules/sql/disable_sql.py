06f4f72d9a97772edbaa0b60e66fb11cc28f7ac0
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-05-29 14:34:37 +0530

    Anki Vector

--- AnkiVector/modules/sql/disable_sql.py
+++ AnkiVector/modules/sql/disable_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, distinct, func
 
-from DaisyX.modules.sql import BASE, SESSION
+from AnkiVector.modules.sql import BASE, SESSION
 
 
 class Disable(BASE):
