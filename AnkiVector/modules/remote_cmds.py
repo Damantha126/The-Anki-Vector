11499d1681f280ca4c0382da2b48286d13080db8
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-05-29 11:29:16 +0530

    Anki Vector

--- AnkiVector/modules/remote_cmds.py
+++ AnkiVector/modules/remote_cmds.py
@@ -2,15 +2,15 @@ from telegram import ChatPermissions, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
-from DaisyX import dispatcher
-from DaisyX.modules.helper_funcs.chat_status import (
+from AnkiVector import dispatcher
+from AnkiVector.modules.helper_funcs.chat_status import (
     bot_admin,
     is_bot_admin,
     is_user_ban_protected,
     is_user_in_chat,
 )
-from DaisyX.modules.helper_funcs.extraction import extract_user_and_text
-from DaisyX.modules.helper_funcs.filters import CustomFilters
+from AnkiVector.modules.helper_funcs.extraction import extract_user_and_text
+from AnkiVector.modules.helper_funcs.filters import CustomFilters
 
 RBAN_ERRORS = {
     "User is an administrator of the chat",
