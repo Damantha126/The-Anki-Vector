e06a1edf7157fe7f785276862b780639319fdf03
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-05-29 10:49:28 +0530

    Anki Vector

--- AnkiVector/modules/gps.py
+++ AnkiVector/modules/gps.py
@@ -2,8 +2,8 @@ from geopy.geocoders import Nominatim
 from telegram import Location, ParseMode
 from telegram.ext import CommandHandler, run_async
 
-from DaisyX import dispatcher
-from DaisyX.modules.helper_funcs.chat_status import user_admin
+from AnkiVector import dispatcher
+from AnkiVector.modules.helper_funcs.chat_status import user_admin
 
 GMAPS_LOC = "https://maps.googleapis.com/maps/api/geocode/json"
 
