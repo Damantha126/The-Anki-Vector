9102afa1c8ca2c199f3b54e106a6d31f6e20dc01
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-05-29 11:19:12 +0530

    Anki Vector

--- AnkiVector/modules/notes.py
+++ AnkiVector/modules/notes.py
@@ -23,14 +23,14 @@ from telegram.ext import (
 from telegram.ext.dispatcher import run_async
 from telegram.utils.helpers import escape_markdown, mention_markdown
 
-import DaisyX.modules.sql.notes_sql as sql
-from DaisyX import DRAGONS, JOIN_LOGGER, LOGGER, SUPPORT_CHAT, dispatcher
-from DaisyX.modules.disable import DisableAbleCommandHandler
-from DaisyX.modules.helper_funcs.chat_status import connection_status, user_admin
-from DaisyX.modules.helper_funcs.handlers import MessageHandlerChecker
-from DaisyX.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from DaisyX.modules.helper_funcs.msg_types import get_note_type
-from DaisyX.modules.helper_funcs.string_handling import escape_invalid_curly_brackets
+import AnkiVector.modules.sql.notes_sql as sql
+from AnkiVector import DRAGONS, JOIN_LOGGER, LOGGER, SUPPORT_CHAT, dispatcher
+from AnkiVector.modules.disable import DisableAbleCommandHandler
+from AnkiVector.modules.helper_funcs.chat_status import connection_status, user_admin
+from AnkiVector.modules.helper_funcs.handlers import MessageHandlerChecker
+from AnkiVector.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from AnkiVector.modules.helper_funcs.msg_types import get_note_type
+from AnkiVector.modules.helper_funcs.string_handling import escape_invalid_curly_brackets
 
 FILE_MATCHER = re.compile(r"^###file_id(!photo)?###:(.*?)(?:\s|$)")
 STICKER_MATCHER = re.compile(r"^###sticker(!photo)?###:")
@@ -542,7 +542,7 @@ A button can be added to a note by using standard markdown link syntax - the lin
 
 """
 
-__mod_name__ = "Notes ✍️"
+__mod_name__ = "Notes"
 
 GET_HANDLER = CommandHandler("get", cmd_get)
 HASH_GET_HANDLER = MessageHandler(Filters.regex(r"^#[^\s]+"), hash_get)
