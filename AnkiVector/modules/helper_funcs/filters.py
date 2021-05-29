fc466654703b3b74f23e3e1e70868f50e0f503bf
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-05-29 14:53:30 +0530

    Anki Vector

--- AnkiVector/modules/helper_funcs/filters.py
+++ AnkiVector/modules/helper_funcs/filters.py
@@ -1,7 +1,7 @@
 from telegram import Message
 from telegram.ext import BaseFilter
 
-from DaisyX import DEMONS, DEV_USERS, DRAGONS
+from AnkiVector import DEMONS, DEV_USERS, DRAGONS
 
 
 class CustomFilters(object):
