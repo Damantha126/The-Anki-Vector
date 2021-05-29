68a5e4e56766fd0ca98d03b786219984a11d4cbf
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-05-29 14:38:53 +0530

    Anki Vector

--- AnkiVector/modules/sql/notes_sql.py
+++ AnkiVector/modules/sql/notes_sql.py
@@ -3,8 +3,8 @@ import threading
 
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText, distinct, func
 
-from DaisyX.modules.helper_funcs.msg_types import Types
-from DaisyX.modules.sql import BASE, SESSION
+from AnkiVector.modules.helper_funcs.msg_types import Types
+from AnkiVector.modules.sql import BASE, SESSION
 
 
 class Notes(BASE):
