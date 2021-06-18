988c1c5443f4719a55c0270c4e4fa24d97cd2f62
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-06-18 18:12:36 +0530

    Anki Vector

--- AnkiVector/modules/upload.py
+++ AnkiVector/modules/upload.py
@@ -181,8 +181,8 @@ if __name__ == "__main__":
     main()
 
 __help__ = """
- ⦁ `/transfersh`*:* reply to a telegram file to upload it on transfersh and get direct download link
- ⦁ `/tmpninja`*:* reply to a telegram file to upload it on tmpninja and get direct download link
+ ⦁ /transfersh : reply to a telegram file to upload it on transfersh and get direct download link
+ ⦁ /tmpninja : reply to a telegram file to upload it on tmpninja and get direct download link
 
 © @Damantha_Jasinghe 🇱🇰
 """
