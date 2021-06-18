89e0a3c677fe15f091ffc5ee49fa4864dcd2d01d
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-06-18 11:53:56 +0530

    Anki Vector

---
+++ AnkiVector/modules/MusicPlay.py
@@ -0,0 +1,69 @@
+__help__ = """
+** • Groups and channels Voice Chat Music Play 🎧  **
+1) Make bot admin (Group and in channel if use cplay)
+2) Start a voice chat
+3) Try /play [song name] for the first time by an admin
+*) If userbot joined enjoy music, If not add @AnkivectorMusicbot to your group and retry
+For Channel Music Play
+1) Make @TheAnkiVectorMusicBot admin of your channel 
+2) Send /userbotjoinchannel in linked group
+3) Now send commands in linked group
+
+** Commands **
+ 
+ ** Groups Music Play **
+- /play: Play song using youtube music
+- /play [yt url] : Play the given yt url
+- /play [reply yo audio]: Play replied audio
+- /dplay: Play song via deezer
+- /splay: Play song via jio saavn
+
+** Playback **
+- /player: Open Settings menu of player
+- /skip: Skips the current track
+- /pause: Pause track
+- /resume: Resumes the paused track
+- /end: Stops media playback
+- /current: Shows the current Playing track
+- /playlist: Shows playlist
+*Player cmd and all other cmds except /play, /current  and /playlist  are only for admins of the group.
+ 
+ 
+ ** Channel Music Play **
+
+** ♦️ For linked group admins only **
+
+- /cplay [song name] - play song you requested
+- /cdplay [song name] - play song you requested via deezer
+- /csplay [song name] - play song you requested via jio saavn
+- /cplaylist - Show now playing list
+- /cccurrent - Show now playing
+- /cplayer - open music player settings panel
+- /cpause - pause song play
+- /cresume - resume song play
+- /cskip - play next song
+- /cend - stop music play
+- /userbotjoinchannel - invite assistant to your chat
+channel is also can be used instead of c ( /cplay = /channelplay )
+
+
+** ♦️ If you donlt like to play in linked group **
+
+1) Get your channel ID.
+2) Create a group with tittle: Channel Music: your_channel_id
+3) Add bot as Channel admin with full perms
+4) Add @AnkivectorMusicbot to the channel as an admin.
+5) Simply send commands in your group.
+
+** More tools **
+
+- /admincache: Updates admin info of your group. Try if bot isn't recognize admin
+- /userbotjoin: Invite @AnkivectorMusicbot Userbot to your chat 
+
+** Assisten **
+@AnkivectorMusicbot
+
+** Bot **
+@TheAnkiVectorMusicBot
+"""
+__mod_name__ = "Music Play"
