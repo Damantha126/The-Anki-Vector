a3ea50356482a9510ea8b8888ba273cd5e2dcaf3
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-06-18 13:04:19 +0530

    Anki Vector

---
+++ AnkiVector/modules/about.py
@@ -0,0 +1,14 @@
+__help__ = """
+** Anki Vector - A Powerful Telegram Group Manager 🎶 **
+
+Powerful Abilities
+
+• Group Voice Chat Music Play ❤️
+
+• File To Link and URL Upload 📂
+
+• Youtube Downloader 🎵
+
+Developer [Damantha🇱🇰](https://t.me/Damantha_Jasinghe)
+"""
+__mod_name__ = "About"
