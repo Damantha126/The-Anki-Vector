c389f3dec531277157353e5be1f65cb8e9fee0c3
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-06-19 12:08:25 +0530

    Anki Vector

--- AnkiVector/__main__.py
+++ AnkiVector/__main__.py
@@ -219,9 +219,10 @@ def start(update: Update, context: CallbackContext):
                 IMPORTED["rules"].send_rules(update, args[0], from_pm=True)
 
         else:
-            update.effective_message.reply_text(
+            first_name = update.effective_user.first_name
+            update.effective_message.reply_animation(
                 BOT_IMG,
-                PM_START_TEXT,
+                caption=PM_START_TEXT,
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,
