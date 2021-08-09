b5b6570413c95223e7b0e6d943891bf37d01ce62
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-08 23:34:22 -0700

    Update blacklistusers.py

--- AnkiVector/modules/blacklistusers.py
+++ AnkiVector/modules/blacklistusers.py
@@ -159,5 +159,5 @@ dispatcher.add_handler(BL_HANDLER)
 dispatcher.add_handler(UNBL_HANDLER)
 dispatcher.add_handler(BLUSERS_HANDLER)
 
-__mod_name__ = "Blacklisting Users"
+__mod_name__ = "Blacklisting Users⚫️"
 __handlers__ = [BL_HANDLER, UNBL_HANDLER, BLUSERS_HANDLER]
