736c150cff2fd870260e8037ef8b7d9123286ee9
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-09 00:35:55 -0700

    Update dbcleanup.py

--- AnkiVector/modules/dbcleanup.py
+++ AnkiVector/modules/dbcleanup.py
@@ -141,11 +141,11 @@ def callback_button(update: Update, context: CallbackContext):
             query.answer("You are not allowed to use this.")
 
 
-DB_CLEANUP_HANDLER = CommandHandler("dbcleanup🧹", dbcleanup)
+DB_CLEANUP_HANDLER = CommandHandler("dbcleanup", dbcleanup)
 BUTTON_HANDLER = CallbackQueryHandler(callback_button, pattern="db_.*")
 
 dispatcher.add_handler(DB_CLEANUP_HANDLER)
 dispatcher.add_handler(BUTTON_HANDLER)
 
-__mod_name__ = "DB Cleanup"
+__mod_name__ = "DB Cleanup🧹"
 __handlers__ = [DB_CLEANUP_HANDLER, BUTTON_HANDLER]
