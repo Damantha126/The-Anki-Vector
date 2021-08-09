891b87195bd30af9d2bd994c232a5c256474d9dc
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-08 23:33:14 -0700

    Update chatbot.py

--- AnkiVector/modules/chatbot.py
+++ AnkiVector/modules/chatbot.py
@@ -170,7 +170,7 @@ dispatcher.add_handler(REMOVE_CHAT_HANDLER)
 dispatcher.add_handler(CHATBOT_HANDLER)
 dispatcher.add_handler(LIST_CB_CHATS_HANDLER)
 
-__mod_name__ = "Chatbot"
+__mod_name__ = "Chatbot🤖"
 __command_list__ = ["addchat", "rmchat", "listaichats"]
 __handlers__ = [
     ADD_CHAT_HANDLER,
