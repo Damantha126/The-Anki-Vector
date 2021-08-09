94388bf75a1ca22f52d615e190d75deb57cc5ebe
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-08 20:25:34 -0700

    Update speed_test.py

--- AnkiVector/modules/speed_test.py
+++ AnkiVector/modules/speed_test.py
@@ -60,6 +60,6 @@ SPEED_TEST_CALLBACKHANDLER = CallbackQueryHandler(
 dispatcher.add_handler(SPEED_TEST_HANDLER)
 dispatcher.add_handler(SPEED_TEST_CALLBACKHANDLER)
 
-__mod_name__ = "SpeedTest"
+__mod_name__ = "SpeedTest☯️"
 __command_list__ = ["speedtest"]
 __handlers__ = [SPEED_TEST_HANDLER, SPEED_TEST_CALLBACKHANDLER]
