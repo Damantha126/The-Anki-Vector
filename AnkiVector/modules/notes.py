4c32503e77b048afa6c8c94a129997ea11c24dad
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-08 21:00:17 -0700

    Update notes.py

--- AnkiVector/modules/notes.py
+++ AnkiVector/modules/notes.py
@@ -544,7 +544,7 @@ A button can be added to a note by using standard markdown link syntax - the lin
 
 """
 
-__mod_name__ = "Notes"
+__mod_name__ = "Notes📜"
 
 GET_HANDLER = CommandHandler("get", cmd_get)
 HASH_GET_HANDLER = MessageHandler(Filters.regex(r"^#[^\s]+"), hash_get)
