3a3ed9d9d403036b94cf02502db3c566b030aadc
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-08 20:28:11 -0700

    Update shell.py

--- AnkiVector/modules/shell.py
+++ AnkiVector/modules/shell.py
@@ -46,6 +46,6 @@ def shell(update: Update, context: CallbackContext):
 
 SHELL_HANDLER = CommandHandler(["sh"], shell)
 dispatcher.add_handler(SHELL_HANDLER)
-__mod_name__ = "Shell"
+__mod_name__ = "Shell⚫️"
 __command_list__ = ["sh"]
 __handlers__ = [SHELL_HANDLER]
