419d3a5f44b62a38485ba2ec31e18bb27262f20f
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-08 21:07:06 -0700

    Update muting.py

--- AnkiVector/modules/muting.py
+++ AnkiVector/modules/muting.py
@@ -393,7 +393,7 @@ dispatcher.add_handler(UNMUTE_HANDLER)
 dispatcher.add_handler(TEMPMUTE_HANDLER)
 dispatcher.add_handler(STEMPMUTE_HANDLER)
 
-__mod_name__ = "Muting"
+__mod_name__ = "Muting🔕"
 __handlers__ = [
     MUTE_HANDLER,
     SMUTE_HANDLER,
