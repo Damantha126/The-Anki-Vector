5005351a37b092f61cce58417a564f4855dbc446
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-09 02:45:28 -0700

    Update about.py

--- AnkiVector/modules/about.py
+++ AnkiVector/modules/about.py
@@ -5,7 +5,7 @@ All Credits & Special thanks 🌞
 
 🌐 First Owners [Yakari music](https://github.com/youtubeslgeekshow/call-music-plus-bot) [Damantha🇱🇰](https://t.me/Damantha_Jasinghe)[Daisy X](https://github.com/TeamDaisyX/DaisyX) 
 
-🌐 I added Force Subscribe for music /song downloader update/ some customizes button and more....
+🌐 I added /nForce Subscribe for music /nsong downloader /n update some customizes button and more....
 
 🌐 Join with us for more education 📖
 
