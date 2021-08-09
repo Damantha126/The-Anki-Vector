035760e672b842a09b2eb1a03d96a03be9e57364
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-08 20:12:06 -0700

    Update upload.py

--- AnkiVector/modules/upload.py
+++ AnkiVector/modules/upload.py
@@ -83,7 +83,7 @@ async def tsh(event):
 
             str(time.time() - start)
             await orta.edit(
-                f"File Successfully Uploaded to TransferSh.\n\nLink 👉 {download_link}\nExpired Date 👉 {final_date}\n\nUploaded by @TheAnkiVectorbot 👸"
+                f"File Successfully Uploaded to TransferSh.\n\nLink 👉 {download_link}\nExpired Date 👉 {final_date}\n\nUploaded by @sl_bot_zone"
             )
         except Exception as e:
             traceback.print_exc()
@@ -118,7 +118,7 @@ async def tmp(event):
 
             str(time.time() - start)
             await orta.edit(
-                f"File Successfully Uploaded to TmpNinja.\n\nLink 👉 {download_link}\n\nUploaded by @TheAnkiVectorbot 👸"
+                f"File Successfully Uploaded to TmpNinja.\n\nLink 👉 {download_link}\n\nUploaded by @sl_bot_zone"
             )
         except Exception as e:
             traceback.print_exc()
@@ -159,7 +159,7 @@ async def up(event):
                 event.chat.id,
                 dosya,
                 force_document=True,
-                caption=f"Uploaded By @TheAnkiVectorbot",
+                caption=f"Uploaded By @sl_bot_zone",
             )
         except Exception as e:
             traceback.print_exc()
@@ -184,7 +184,7 @@ __help__ = """
  ⦁ /transfersh : reply to a telegram file to upload it on transfersh and get direct download link
  ⦁ /tmpninja : reply to a telegram file to upload it on tmpninja and get direct download link
 
-© @Damantha_Jasinghe 🇱🇰
+© @sl_bot_zone🇱🇰
 """
 
-__mod_name__ = "File To Link"
+__mod_name__ = "File To Link🔗"
