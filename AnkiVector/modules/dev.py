c3607709cc04166cc708c2cf0841029f22a3fdca
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-08 23:19:59 -0700

    Update dev.py

--- AnkiVector/modules/dev.py
+++ AnkiVector/modules/dev.py
@@ -67,5 +67,5 @@ dispatcher.add_handler(LEAVE_HANDLER)
 dispatcher.add_handler(GITPULL_HANDLER)
 dispatcher.add_handler(RESTART_HANDLER)
 
-__mod_name__ = "Dev"
+__mod_name__ = "Dev👨‍💻"
 __handlers__ = [LEAVE_HANDLER, GITPULL_HANDLER, RESTART_HANDLER]
