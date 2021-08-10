3266e8b2a605ef373fd5705c4e6a432c274e8874
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-09 19:30:22 -0700

    Update __main__.py

--- AnkiVector/__main__.py
+++ AnkiVector/__main__.py
@@ -242,7 +242,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_text(
-            "I'm awake already!\n<b>Haven't slept since:</b> <code>{}</code>".format(
+            "I'm awake already!\n<b>Haven't slept since:</b> <code>{}</code>🌞".format(
                 uptime
             ),
             parse_mode=ParseMode.HTML,
