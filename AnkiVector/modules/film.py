b7ec81eddfe32cdb0f50d047ce9607e5d1c5772a
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-09 18:32:50 -0700

    Create film.py

---
+++ AnkiVector/modules/film.py
@@ -0,0 +1,6 @@
+__help__ = """
+** You can search any Film useing this Bot **
+❍ Use this format @szfilmbot film name 
+❍ Or you can use Direct this bot in Pm @szfilmbot
+"""
+__mod_name__ = "Film 🎞"
