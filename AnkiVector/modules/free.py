542ffa22960525c0d0a8c4feee0b63ff9c569d9c
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-10 00:11:06 -0700

    Create free.py

---
+++ AnkiVector/modules/free.py
@@ -0,0 +1,7 @@
+__help__ = """
+** You can Get Free internet file useing this bot**
+❍ Use this format @EHIfilebot File name [ehi/sks/httpetc...]
+❍ Join this group for more information @LIONHEARTn..
+❍ Use this Video [video](https://www.youtube.com/channel/UCvYfJcTr8RY72dIapzMqFQA)
+"""
+__mod_name__ = "Free internet 💡 "
