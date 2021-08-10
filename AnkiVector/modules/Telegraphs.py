afb6174da789587345f21496949b0e379b6e2c38
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-09 18:49:26 -0700

    Update Telegraphs.py

--- AnkiVector/modules/Telegraphs.py
+++ AnkiVector/modules/Telegraphs.py
@@ -1,5 +1,5 @@
 __help__ = """
-** You can search any Modapp useing this Bot **
+** You can Upload telegraph  useing this Bot **
 ❍ /telegraph <reply to jpg, jpeg, png, gif or mp4>: upload to telegraph.
 """
 __mod_name__ = "Telegraph💌"
