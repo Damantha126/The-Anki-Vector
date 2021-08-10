0be9e8404dea8ed43e3b42d257c1565a8073ce87
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-09 18:37:44 -0700

    Create Modapp.py

---
+++ AnkiVector/modules/Modapp.py
@@ -0,0 +1,6 @@
+__help__ = """
+** You can search any Modapp useing this Bot **
+❍ Use this format @szmodappbot App name
+❍ Or you can use Direct this bot in Pm @szmodappbot
+"""
+__mod_name__ = "Mod app 📲"
