56bb9a9f3217f4af19e545b4b776cb7bf33264f8
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-11 01:45:27 -0700

    Create imaget.py

---
+++ AnkiVector/modules/imaget.py
@@ -0,0 +1,9 @@
+__help__ = """
+** You can use this module to image search etc**
+❍ /img <text>: perform a image search
+❍ /getqr <reply to image>: Read QR code
+❍ /makeqr <reply to text>: Make QR code
+"""
+__mod_name__ = "Image☯️ "
+
+
