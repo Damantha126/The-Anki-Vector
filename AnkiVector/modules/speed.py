c0e150442696f79b5d58d29b421cd248a7669340
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-11 00:02:51 -0700

    Update speed.py

--- AnkiVector/modules/speed.py
+++ AnkiVector/modules/speed.py
@@ -1,8 +1,8 @@
 __help__ = """
 ** You can sea my internet speed useing this button  **
 ❍ Type /speedtest and send it
-❍ ping 
-❍ ding 
-❍ pingall
+❍ ping /ping
+❍ ding /ding
+❍ pingall /pingall
 """
 __mod_name__ = "speedtest💥"
