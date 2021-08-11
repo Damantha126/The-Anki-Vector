fb500a6e95316163a0028b74a34f9b118c72f32b
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-11 01:52:55 -0700

    Create git.py

---
+++ AnkiVector/modules/git.py
@@ -0,0 +1,7 @@
+__help__ = """
+@szrosebot 🇱🇰
+I will give information about github profile and repository ..  
+ ❍ /git <username>: Get information about a GitHub user.
+ ❍ /repo <name> : Get information about a GitHub  repository .
+"""
+__mod_name__ = "Github ☀️ "
