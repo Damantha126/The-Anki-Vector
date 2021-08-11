d8998a2b57a9099727ec99db59e0f219b9da25e3
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-11 01:39:22 -0700

    Create bluete.py

---
+++ AnkiVector/modules/bluete.py
@@ -0,0 +1,9 @@
+__help__ = """
+@szrosebot 🇱🇰
+Blue text cleaner removed any made up commands that people send in your chat.
+ ❍ /cleanblue <on/off/yes/no>: clean commands after sending
+ ❍ /ignoreblue <word>: prevent auto cleaning of the command
+ ❍ /unignoreblue <word>: remove prevent auto cleaning of the command
+ ❍ /listblue: list currently whitelisted commands
+"""
+__mod_name__ = "B Text🛑"
